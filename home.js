4446ce49b862963c0483e3dc681e7d58e4ac14c5
reddy <reddys.jayadeep@gmail.com>
2020-07-14 11:11:49 +0530

    changes

--- home.js
+++ home.js
@@ -1,8 +1,10 @@
 var userid;
+var currentdate;
 $( document ).ready(function() {
     console.log( "ready!" );
    currentUser();
-   
+   currentdate = new Date();
+   console.log(currentdate);
 });
 
 var newArray = [];
@@ -54,7 +56,7 @@ function openNav() {
  function getTournaments(gameId){
     var tourRef = db.collection("Tournaments");
 
-tourRef.where("gameId", "==", gameId).get().then(function(querySnapshot) {
+tourRef.where("gameId", "==", gameId).where("endTime", ">", currentdate).get().then(function(querySnapshot) {
     var data = [];
     querySnapshot.forEach(function(doc) {
         // doc.data() is never undefined for query doc snapshots
@@ -69,7 +71,9 @@ tourRef.where("gameId", "==", gameId).get().then(function(querySnapshot) {
  //   newArray.push(data);
     //  console.log(newArray);
     for(var i = 0; i < data.length;i++){
-        var gamename = data[0].gameName
+        var gamename = data[0].gameName;
+        var endtime = data[0].endTime;
+        console.log(endtime.seconds);
       $("."+gamename+'').append('<div class="f1ma0gg7"><div class="contestCardDeposit500" style="display: flex; height: 100%; position: relative;"><div class="f1ylnkmr"><div class="fshmzbu">Prize</div><div class="f1wmjatj"><img class="fpliu47"src="https://static.gamezop.com/peach/assets/img/multiple-rupee-note.svg" alt=""><spanstyle="margin-left: 7px;">'+data[i].Prize+'</span></div></div><div class="f1tq6dag" style="left: 37%;"><div class="f1mvdm5l" style="padding-top: 10px;"><img class="f1hn91pk"src="https://static.gamezop.com/peach/assets/img/faceless-player.svg" alt=""><div class="f1omk9ww">'+data[i].joined+' <span style="padding-left: 2px; padding-right: 2px;">/</span>'+data[i].maxLimit+' </div></div><div class="f1mvdm5l" style="padding-left: 8px; padding-bottom: 5px;"><img class="f1n7oroy"src="https://static.gamezop.com/peach/assets/img/timer.svg" alt=""><div class="clock_img">'+data[i].starttime+'-'+data[i].endtime+'</div></div></div><a href="http://localhost:7000/tournament.html?tournamentId='+data[i].tournamentId+'"><div class="f1dfiwkb"><img class="fo34vq9" src="https://static.gamezop.com/peach/assets/img/multiple-rupee-note.svg" alt=""><span>'+data[i].entryFee+'</span></div></a></div></div>');
     }
       
