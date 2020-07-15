f3ebc38c9987899dea68cc154841e32525f74845
kashyap-dev <parchakashyap@gmail.com>
2020-07-15 17:26:27 +0530

    commit
    segment buttons

--- home.js
+++ home.js
@@ -72,7 +72,8 @@ tourRef.where("gameId", "==", gameId).where("endTime", ">", currentdate).get().t
     for(var i = 0; i < data.length;i++){
         var gamename = data[0].gameName;
         var endtime = data[i].endTime.toDate();
-      //  var endtime = moment(endtime).endOf('day').fromNow();
+       //var endtime = moment(endtime).endOf('day').fromNow();
+        //var currentTime= moment().format()
         var endtime = moment(endtime).calendar();
       $("."+gamename+'').append('<div class="f1ma0gg7"><div class="contestCardDeposit500" style="display: flex; height: 100%; position: relative;"><div class="f1ylnkmr"><div class="fshmzbu">Prize</div><div class="f1wmjatj"><img class="fpliu47"src="https://static.gamezop.com/peach/assets/img/multiple-rupee-note.svg" alt=""><spanstyle="margin-left: 7px;">'+data[i].Prize+'</span></div></div><div class="f1tq6dag" style="left: 37%;"><div class="f1mvdm5l" style="padding-top: 10px;"><img class="f1hn91pk"src="https://static.gamezop.com/peach/assets/img/faceless-player.svg" alt=""><div class="f1omk9ww">'+data[i].joined+' <span style="padding-left: 2px; padding-right: 2px;">/</span>'+data[i].maxLimit+' </div></div><div class="f1mvdm5l" style="padding-left: 8px; padding-bottom: 5px;"><img class="f1n7oroy"src="https://static.gamezop.com/peach/assets/img/timer.svg" alt=""><div class="clock_img">Ends '+endtime+'</div></div></div><a href="http://localhost:7000/tournament.html?tournamentId='+data[i].tournamentId+'"><div class="f1dfiwkb"><img class="fo34vq9" src="https://static.gamezop.com/peach/assets/img/multiple-rupee-note.svg" alt=""><span>'+data[i].entryFee+'</span></div></a></div></div>');
     }
